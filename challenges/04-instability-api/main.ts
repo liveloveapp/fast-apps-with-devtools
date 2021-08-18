22ee68396ce2d90f28e48361ce5aabd4bee7d371
Brian Love <brian@brianflove.com>
2021-08-18 10:14:22 -0700

    04 instability api

--- challenges/04-instability-api/main.ts
+++ challenges/04-instability-api/main.ts
@@ -1,2 +1,19 @@
 import { Warning } from '../../components';
 new Warning();
+
+/** LayoutShift API doesn't exist in lib.dom.ts ?!? */
+interface LayoutShift extends PerformanceEntry {
+  hadRecentInput: boolean;
+  value: number;
+}
+
+let cls = 0;
+new PerformanceObserver((entryList) => {
+  for (const entry of entryList.getEntries()) {
+    const layoutShift = entry as LayoutShift;
+    if (!layoutShift.hadRecentInput) {
+      cls += layoutShift.value;
+      console.log('Current CLS value:', cls, entry);
+    }
+  }
+}).observe({ type: 'layout-shift', buffered: true });
